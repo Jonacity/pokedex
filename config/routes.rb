bb1485443c2ec874579f7d1276c01112c593ba93
jonathan sitti <sitti.jonathan@gmail.com>
2018-07-31 19:44:30 +0200

    design started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'pokemons/index'
+  root to: 'pokemons#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
